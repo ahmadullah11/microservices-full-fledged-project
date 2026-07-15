7bcf1f370b340a1784d54b376a6c30c4540448ba
ahmadullah11 <ahmadullahk543@gmail.com>
2026-07-16 00:55:36 +0500

    chore(deps): add http-status-codes package

--- server/services/auth-service/src/config/db.ts
+++ server/services/auth-service/src/config/db.ts
@@ -1,4 +1,4 @@
 import { env } from "./env.js"
 import { drizzle } from 'drizzle-orm/node-postgres';
 
-const db = drizzle(env.DATABASE_URL!)
+export const db = drizzle(env.DATABASE_URL!)
--- server/services/auth-service/src/controller/index.ts
+++ server/services/auth-service/src/controller/index.ts
@@ -1,5 +1,27 @@
 import type { Request, Response} from "express"
+import { SignUpSchema } from "../validation/index.js"
+import { AppError } from "../error/AppError.js";
+import { StatusCodes } from "http-status-codes";
+import { email } from 'zod';
+import { db } from "../config/db.js";
+import { eq } from "drizzle-orm";
+import { usersTable } from "../schema/index.js";
 
 export const sighnUpController = async (req: Request, res: Response ) => {
+    const schema = SignUpSchema.safeParse(req.body);
 
+    if(!schema){
+        throw new AppError(schema.error.message, StatusCodes.NOT_ACCEPTABLE)
+    }
+
+    const { name, email, password } = schema.data;
+
+    const userExist = await 
+    db.select()
+    .from(usersTable)
+    .where(eq(usersTable.email, email));
+
+    if(userExist){
+        throw new AppError("User Already Exist", StatusCodes.CONFLICT)
+    }
 }
