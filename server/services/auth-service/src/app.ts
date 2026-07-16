e8d6edf91888fba8a5d2bf2408d023152dee2931
ahmadullah11 <ahmadullahk543@gmail.com>
2026-07-16 14:16:10 +0500

    feat(routes): add authentication routes

--- server/services/auth-service/src/app.ts
+++ server/services/auth-service/src/app.ts
@@ -1,5 +1,13 @@
 import express from "express";
+import cors from 'cors';
+import authRoutes from "./routes/index.js"
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use("api/auth", authRoutes)
 
 export default app;
