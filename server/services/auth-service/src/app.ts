ffa52f4856ae1fa91a4973755501f0b489a2cc13
ahmadullah11 <ahmadullahk543@gmail.com>
2026-07-16 14:22:33 +0500

    feat(docker): add Dockerfile for auth service

--- server/services/auth-service/src/app.ts
+++ server/services/auth-service/src/app.ts
@@ -1,13 +1,20 @@
-import express from "express";
+import express, { type Request, type Response } from 'express';
 import cors from 'cors';
-import authRoutes from "./routes/index.js"
+import authRoutes from './routes/index.js';
+// import { errorHandler } from './middleware/index.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use("api/auth", authRoutes)
+app.get('/health', (req: Request, res: Response) => {
+  res.send('OK');
+});
+
+app.use('/api/auth', authRoutes);
 
 export default app;
+
+// app.use(errorHandler);
--- server/services/auth-service/src/validation/index.ts
+++ server/services/auth-service/src/validation/index.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod';
 
 export const SignUpSchema = z.object({
-    name: z.string().min(3),
-    email: z.email().min(3),
-    password: z.string().min(6)
-})
+  name: z.string().min(3),
+  email: z.email().min(3),
+  password: z.string().min(8),
+});
 
 export const LoginSchema = z.object({
-    email: z.email().min(3),
-    password: z.string().min(6)
-})
+  email: z.email().min(3),
+  password: z.string().min(8),
+});
